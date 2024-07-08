2e0088fbc4a8827c35b12ebd92ec6e469c129038
Iyad Jamaan soliman Imaslam <142739549+IyadJamaa@users.noreply.github.com>
2024-07-08 20:13:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,13 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 const app = express();
 const port = 5001;
 //middlewares
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["farsha-front","https://farsha-front.vercel.app/login"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.use(bodyParser.json());
